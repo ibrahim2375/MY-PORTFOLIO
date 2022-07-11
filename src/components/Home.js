f872a10ae66c45cbb8edf0222394b1e93ea6e4f2
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-07-11 04:59:44 +0200

    up..

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,12 @@ import '../css/home.css'
 // get data 
 import data from '../data/myinfo.json'
 import img from '../image/profile.jpg'
+import { Link } from 'react-router-dom'
 // import img from '../image/94632c52-6791-45ef-bea6-e7c04c0ed7a4-removebg-preview.png'
 
 // import { Typewriter } from 'react-simple-typewriter';
 function Home() {
-  const { txtcolor,  } = useContext(MainContext);
+  const { txtcolor, } = useContext(MainContext);
   return (
     <div className="home">
 
@@ -18,7 +19,8 @@ function Home() {
         <h3>Hello There , My name is <span className="headline">{data.personal.name}</span></h3>
         <p>I`m  a  <span className="headline">{data.personal.job} , {data.personal.job2} </span></p>
         <p>{data.personal.description}</p>
-        <a href="/contact" className="btn">Contact</a>
+        {/* <a href="/contact" className="btn">Contact</a> */}
+        <Link to="/contact" className="btn"  >Contact</Link>
       </div>
       <div className="img">
         <img src={img} alt="Profile Img" />
